ffe6f788fe4fea14dcae4f30d4f9149b90634cff
Rajshree <rajshreeakm@gmail.com>
2025-05-06 11:56:19 +0530

    Documented the solution for making it of productive quality

--- src/GrossCalculator.java
+++ src/GrossCalculator.java
@@ -1,7 +1,38 @@
+
+/**
+ * Problem 2:
+ * A supermarket sells thousands of products. Every product is properly packaged and
+ * the product packaging shows the total price a customer must pay for the product.
+ * The total product price includes taxes and the rate of taxation varies from product to
+ * product. For legal reasons, the supermarket must provide invoices to every customer
+ * that show the price of each product before taxes, the tax rate and the tax amount.
+ * Write a program that takes the net price of a product including taxes and the tax rate,
+ * and computes the gross price before taxes. The table below shows some examples
+ * for the expected output of the program.
+ *
+ * | Net Price  |  Tax Rate   |   Gross Price |
+ * | 99.95      |   0.12      |     89.24     |
+ * | 49,999.00  |   0.28      |    39,061.72  |
+ * | 720.00     |   0.05      |    685.71     |
+ * | 45.00      |   0.00      |    45.00      |
+ *
+ */
+
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class GrossCalculator {
+
+
+    /**
+     * Returns the gross price based on the given net price and tax rate.
+     * Negative value(s) of net price or tax rate gives invalid input error as they can't be negative.
+     *
+     * @param netPrice the input net price
+     * @param taxRate  the input tax rate
+     * @return the calculated gross price
+     */
 
     public static double calculateGross(double netPrice, double taxRate){
         return  netPrice / (1+taxRate);
