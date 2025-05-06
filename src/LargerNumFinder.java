83945efe6b400d1094c475e0a16754d31ef3e585
Rajshree <rajshreeakm@gmail.com>
2025-05-06 20:22:55 +0530

    Updated the problem 2 solution to ensure the output as a plain number instead of scientific notation

--- src/LargerNumFinder.java
+++ src/LargerNumFinder.java
@@ -1,6 +1,21 @@
+/*
+* Problem 3
+Write a program that accepts two numbers and returns the larger of the two.
+*
+* */
+
+
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class LargerNumFinder {
+
+    /**
+     * Returns the larger of the two provided double values.
+     * @param a First number
+     * @param b Second number
+     * @return The larger number
+     */
 
     public static double getLargerNum(double a, double b){
         return (a>=b)?a:b;
@@ -8,6 +23,7 @@ public class LargerNumFinder {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.################");
 
         System.out.print("Enter the first number: ");
         if(!sc.hasNextDouble()){
@@ -27,7 +43,7 @@ public class LargerNumFinder {
         double num2 = sc.nextDouble();
 
         double largerNum = getLargerNum(num1,num2);
-        System.out.println("The larger number is: "+largerNum);
+        System.out.println("The larger number is: "+df.format(largerNum));
 
         sc.close();
     }
